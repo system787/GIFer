374f44c955bb905729e9014abe90749d9a5763ab
Vincent Hoang <vietnhoang7@gmail.com>
2020-07-31 00:35:20 -0700

    Build functions to extract video and image urls from LivePhotos

--- GIFer/Controllers/LivePhotoConverter.swift
+++ GIFer/Controllers/LivePhotoConverter.swift
@@ -24,7 +24,7 @@ class LivePhotoConverter {
     var delegate: LivePhotoConverterDelegate?
     
     // MARK: - Computed Properties
-    private lazy var cacheDir: URL? = {
+    private lazy var cacheURL: URL? = {
         if let cacheURL = try? FileManager.default.url(for: .cachesDirectory, in: .userDomainMask, appropriateFor: nil, create: false) {
             let domainURL = cacheURL.appendingPathComponent("com.vincenthoang.GIFer", isDirectory: true)
             
@@ -38,9 +38,8 @@ class LivePhotoConverter {
         return nil
     }()
     
-    // MARK: - Async Queue
-    private let queue = DispatchQueue(label: "\(DOMAIN_NAME)Queue")
-    
+    private static let queue = DispatchQueue(label: "\(DOMAIN_NAME)Queue", attributes: .concurrent)
+
     // MARK: - initializer/deinitializer
     private init() {
         // TODO
@@ -49,6 +48,14 @@ class LivePhotoConverter {
     deinit {
         // TODO
     }
+    
+    // MARK: - Public Functions
+    public class func getContents(from livePhoto: PHLivePhoto, completion: @escaping (LivePhotoContents?) -> Void) {
+        queue.async {
+            shared.getContents(from: livePhoto, completion: completion)
+        }
+    }
+    
     
     // MARK: - Private Functions
     
@@ -73,17 +80,78 @@ class LivePhotoConverter {
                 //gifURL is set to nil if failed or stopped
         })
         
+    }
+    
+    private func getContents(from livePhoto: PHLivePhoto, to directoryURL: URL, completion: @escaping (LivePhotoContents?) -> Void) {
+        let resources = PHAssetResource.assetResources(for: livePhoto)
+        var livePhotoImageURL: URL?
+        var livePhotoVideoURL: URL?
         
-        let gifURL = URL(fileURLWithPath: "\(fileName).gif", relativeTo: getDocumentsDirectory())
-        delegate?.videoToGIFComplete(gifURL)
+        let dispatchGroup = DispatchGroup()
+        
+        let requestOptions = PHAssetResourceRequestOptions()
+        requestOptions.isNetworkAccessAllowed = true
+        
+        for resource in resources {
+            let buffer = NSMutableData()
+            
+            dispatchGroup.enter()
+            PHAssetResourceManager.default().requestData(for: resource,
+                                                         options: requestOptions,
+                                                         dataReceivedHandler: { data in buffer.append(data) },
+                                                         completionHandler: { error in
+                                                            if error == nil {
+                                                                if resource.type == .pairedVideo {
+                                                                    livePhotoVideoURL = self.saveAsset(resource: resource, to: directoryURL, data: buffer as Data)
+                                                                } else {
+                                                                    livePhotoImageURL = self.saveAsset(resource: resource, to: directoryURL, data: buffer as Data)
+                                                                }
+                                                            } else {
+                                                                NSLog("\(error ?? NSError())")
+                                                            }
+            })
+            dispatchGroup.leave()
+        }
+        dispatchGroup.notify(queue: DispatchQueue.main) {
+            guard let photoURL = livePhotoImageURL, let videoURL = livePhotoVideoURL else {
+                completion(nil)
+                return
+            }
+            completion((photoURL, videoURL))
+        }
+    }
+    
+    private func getContents(from livePhoto: PHLivePhoto, completion: @escaping (LivePhotoContents?) -> Void) {
+        if let cache = cacheURL {
+            getContents(from: livePhoto, to: cache, completion: completion)
+        }
     }
     
     // MARK: - Utility
-    
     private func getDocumentsDirectory() -> URL {
         let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
         let documentsDirectory = paths[0]
         return documentsDirectory
+    }
+    
+    private func saveAsset(resource: PHAssetResource, to directoryURL: URL, data: Data) -> URL? {
+        let fileExtension = UTTypeCopyPreferredTagWithClass(resource.uniformTypeIdentifier as CFString,kUTTagClassFilenameExtension)?.takeRetainedValue()
+        
+        guard let unwrappedExtension = fileExtension else {
+            return nil
+        }
+        
+        var fileURL = directoryURL.appendingPathComponent(UUID().uuidString)
+        fileURL = fileURL.appendingPathExtension(unwrappedExtension as String)
+        
+        do {
+            try data.write(to: fileURL, options: [Data.WritingOptions.atomicWrite])
+        } catch {
+            NSLog("Error writing data to file path \(fileURL.absoluteString)")
+            return nil
+        }
+        
+        return fileURL
     }
     
 }
